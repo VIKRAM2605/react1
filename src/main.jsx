762533d76f44f459807086ca265648ea219b9e06
VIKRAM2605 <vikrams.al24@bitsathy.ac.in>
2024-12-07 09:46:42 +0530

    setup gh-pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter,Routes,Route} from "react"
 import './index.css'
 import Header from './Header'
 import App from './App'
